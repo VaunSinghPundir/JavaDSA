3c88d06daddec69a63215632940448befedd177a
Mona Lisa <vaunsingh814@gmail.com>
2022-09-21 03:06:41 +0530

    Array

--- src/Basic/array.java
+++ src/Basic/array.java
@@ -7,6 +7,7 @@ public class array {
         flat [0][0] = 101;
         flat [0][1] = 102;
         flat [0][2] = 103;
+        flat [0][2] = 104;
         flat [1][0] = 201;
         flat [1][1] = 202;
         flat [1][2] = 203;
